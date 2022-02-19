a2a81335426327a81bf30ee10b490a53f6a3ecbf
Adrià <51267881+AdriiE17@users.noreply.github.com>
2022-02-19 23:12:52 +0100

    added some features

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let sliderIndividual = document.querySelectorAll(".slider-content");
 let counter = 1;
 let width = sliderIndividual[0].clientWidth;
 
-window.addEventListener("resize", function(){
+
+window.addEventListener("resize", e=>{
     width = sliderIndividual[0].clientWidth;
 })
 
@@ -14,11 +15,9 @@ setInterval(()=>{
     slider.style.transition = "transform 1s";
     counter++;
 
-    if(counter === sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            counter=1;
-        },1000);
+    if(counter === sliderIndividual.length + 1){
+        slider.style.transform = "translate(0px)";
+        counter=1;
     }
+
 }, 5000);
