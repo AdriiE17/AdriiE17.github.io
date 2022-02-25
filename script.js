fb7d756db7a54376501b3481b6026d7c56139097
Adrià <51267881+AdriiE17@users.noreply.github.com>
2022-02-25 23:22:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-navigator.vibrate(5000)
-
 const main_slider = document.querySelector(".main-slider");
 const slider = document.querySelector(".slider-container");
 const slider_individual = document.querySelectorAll(".slider-content");
